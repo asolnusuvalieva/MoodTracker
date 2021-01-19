d39a9d3865e2e7bb6f7bd7f0619dcd727d564856
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-01-19 18:17:55 +0600

    Initial Commit

---
+++ MoodTracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MoodTracker
+//
+//  Created by Asol on 1/19/21.
+//  Copyright © 2021 Asol. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
