8cdfe083d2039a14b88f17dd3554b03a7ff5fe8f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-01-27 19:18:25 +0600

    Advance the implementation of the cancel() action in the CategoryViewController

--- MoodTracker/View Controllers/CategoryViewController.swift
+++ MoodTracker/View Controllers/CategoryViewController.swift
@@ -73,7 +73,16 @@ class CategoryViewController: UIViewController, UITextFieldDelegate, UIColorPick
 
     // MARK: - Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddCategoryMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddCategoryMode{
+            dismiss(animated: true, completion: nil)
+        }else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }else{
+            fatalError("The CategoryViewController is not inside a navigation controller.")
+        }
     }
     
    // This method lets you configure a view controller before it's presented.
